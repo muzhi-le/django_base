a1e8f2903f398e400820692ebdc594d4a2260af3
muzhi-le <1163648924@qq.com>
2021-08-17 16:01:13 +0800

    传递form表单数据

--- bookmanage03/book/urls.py
+++ bookmanage03/book/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from book.views import create_book, shop
+from book.views import create_book, shop, register
 
 urlpatterns = [
     path("create/", create_book),
     path("<city_id>/<shop_id>/", shop),
+    path("register/", register),
 ]
--- bookmanage03/book/views.py
+++ bookmanage03/book/views.py
@@ -15,7 +15,15 @@ def create_book(request):
     return HttpResponse("create")
 
 def shop(request, city_id, shop_id):
-    # http://127.0.0.1:8000/a/11/?order=readcount&page=1&order=commentcount
+
+    """
+    查询字符串
+        http://127.0.0.1:8000/a/11/?order=readcount&page=1&order=commentcount
+        url 以 ? 号为分隔， 分为两部分
+        ?号前边为请求路径
+        ?号后边为查询字符串，查询字符串类似于字典，key=value，多个数据采用&拼接
+    """
+
     query_params = request.GET   # < QueryDict: {'order': ['readcount']} >
     # print(query_params)
     # order = query_params.get('order')   # readcount
@@ -26,3 +34,9 @@ def shop(request, city_id, shop_id):
     print(order)
 
     return HttpResponse('aaa')
+
+def register(request):
+
+    data = request.POST    # 接受用户传递过来的请求
+    print(data)
+    return HttpResponse("ok")
